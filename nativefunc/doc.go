897bb24b422eadf4aa3fa54304da555746763c58
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-12-15 05:02:21 +0900

    doc: update GoDoc

--- nativefunc/doc.go
+++ nativefunc/doc.go
@@ -1,6 +1,7 @@
 // Package nativefunc ports several Go's Standard libraries functions to google/go-jsonnet's  Native Functions.
 // The following functions are supported.
 //
+//   - "path/filepath.base"
 //   - "regex.match"
 //   - "strings.contains"
 //   - "strings.trimPrefix"
