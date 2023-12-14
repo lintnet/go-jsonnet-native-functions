7bd0842bd0a01ffb66c326ed6fedb2955426baa1
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-12-15 04:52:46 +0900

    docs: add a link to GoDoc (#11)
    * docs: add a link to GoDoc
    
    * style: update GoDoc
    
    * docs: update GoDoc

--- nativefunc/doc.go
+++ nativefunc/doc.go
@@ -1,2 +1,8 @@
 // Package nativefunc ports several Go's Standard libraries functions to google/go-jsonnet's  Native Functions.
+// The following functions are supported.
+//
+//   - "regex.match"
+//   - "strings.contains"
+//   - "strings.trimPrefix"
+//   - "strings.trimSpace"
 package nativefunc
--- nativefunc/funcs.go
+++ nativefunc/funcs.go
@@ -16,12 +16,16 @@ var funcList = []func() *jsonnet.NativeFunction{ //nolint:gochecknoglobals
 	trimSpace,
 }
 
+// SetAll sets all Native Functions of this package to VM.
 func SetAll(vm *jsonnet.VM) {
 	for _, fc := range funcList {
 		vm.NativeFunction(fc())
 	}
 }
 
+// Set sets given Native Functions to VM.
+// names is a list of Native Function names such as "strings.contains".
+// If a given Native Function isn't found, the error ErrUnknownFunction is returned.
 func Set(vm *jsonnet.VM, names ...string) error {
 	allFuncs := make(map[string]*jsonnet.NativeFunction, len(funcList))
 	for _, fc := range funcList {
