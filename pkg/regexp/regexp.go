7e36e5d836fc03624fdf70eb11a589fc78f99fb9
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-05-31 14:04:45 +0900

    feat: add regxp.MatchStringByArray

--- pkg/regexp/regexp.go
+++ pkg/regexp/regexp.go
@@ -25,3 +25,33 @@ func MatchString(name string) *jsonnet.NativeFunction {
 		},
 	}
 }
+
+func MatchStringByArray(name string) *jsonnet.NativeFunction {
+	return &jsonnet.NativeFunction{
+		Name:   name,
+		Params: ast.Identifiers{"pattern", "s"},
+		Func: func(s []interface{}) (interface{}, error) {
+			pattern, ok := s[0].(string)
+			if !ok {
+				return []any{
+					false, fmt.Sprintf("pattern must be a string: %v", s[0]),
+				}, nil
+			}
+			s1, ok := s[1].(string)
+			if !ok {
+				return []any{
+					false, fmt.Sprintf("s must be a string: %v", s[1]),
+				}, nil
+			}
+			a, err := regexp.MatchString(pattern, s1)
+			if err != nil {
+				return []any{
+					a, err.Error(),
+				}, nil
+			}
+			return []any{
+				a, nil,
+			}, nil
+		},
+	}
+}
--- pkg/regexp/regexp_test.go
+++ pkg/regexp/regexp_test.go
@@ -1,7 +1,9 @@
 package regexp_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -9,7 +11,7 @@ import (
 	"github.com/lintnet/go-jsonnet-native-functions/pkg/regexp"
 )
 
-func TestMatch(t *testing.T) {
+func TestMatchString(t *testing.T) {
 	t.Parallel()
 	data := []struct {
 		name    string
@@ -56,6 +58,61 @@ func TestMatch(t *testing.T) {
 			trimmedResult := strings.TrimSpace(result)
 			if trimmedResult != d.exp {
 				t.Fatalf(`wanted "%s", got "%s"`, d.exp, trimmedResult)
+			}
+		})
+	}
+}
+
+func TestMatchStringByArray(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		name    string
+		pattern string
+		s       string
+		exp     []any
+		isErr   bool
+	}{
+		{
+			name:    "true",
+			pattern: "l{2}",
+			s:       "hello",
+			exp:     []any{true, nil},
+		},
+		{
+			name:    "false",
+			pattern: "a{2}",
+			s:       "hello",
+			exp:     []any{false, nil},
+		},
+		{
+			name:    "invalid regular expression",
+			pattern: "*",
+			s:       "hello",
+			exp:     []any{false, "error parsing regexp: missing argument to repetition operator: `*`"},
+		},
+	}
+	vm := jsonnet.MakeVM()
+	vm.NativeFunction(regexp.MatchStringByArray("regexp.match"))
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			t.Parallel()
+			code := fmt.Sprintf(`std.native("regexp.match")("%s", "%s")`, d.pattern, d.s)
+			result, err := vm.EvaluateAnonymousSnippet("test.jsonnet", code)
+			if err != nil {
+				if d.isErr {
+					return
+				}
+				t.Fatal(err)
+			}
+			if d.isErr {
+				t.Fatal("error must be returned")
+			}
+			var a any
+			if err := json.Unmarshal([]byte(result), &a); err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(a, d.exp) {
+				t.Fatalf(`wanted "%v", got "%v"`, d.exp, a)
 			}
 		})
 	}
